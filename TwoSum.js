156b3981fe950395fab869d2bc2d15e40aca2abb
Hamza Darbani <76852492+Hamza-Dbn@users.noreply.github.com>
2022-06-25 22:11:36 +0200

    Create TwoSum.js

---
+++ TwoSum.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+    
+var twoSum = function(nums, target) {
+  const previousValues = {}
+  for (let i = 0; i < nums.length; i++) {
+    const currentValue = nums[i]
+    const neededValue = target - currentValue
+    if (previousValues[neededValue] != null) {
+      return [previousValues[neededValue], i]
+    } else {
+      previousValues[currentValue] = i
+    }
+  }
+
+	return []
+}
